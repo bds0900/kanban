8a6d7e6127142727898910052e7f9c1ffd624e58
bds0900 <Dbeak9336@conestogac.on.ca>
2020-10-01 22:20:26 -0400

    Update workstation simulation

--- Workstation Simulation/Simulation.Designer.cs
+++ Workstation Simulation/Simulation.Designer.cs
@@ -29,12 +29,135 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ConnBtn = new System.Windows.Forms.Button();
+            this.ActivateBtn = new System.Windows.Forms.Button();
+            this.DeactivateBtn = new System.Windows.Forms.Button();
+            this.EmployeeComboBox = new System.Windows.Forms.ComboBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.RefreshBtn = new System.Windows.Forms.Button();
+            this.AssemblyLines = new System.Windows.Forms.ListBox();
+            this.EmployeeList = new System.Windows.Forms.ListBox();
+            this.ActivatedLines = new System.Windows.Forms.ListBox();
+            this.WorkSpeed = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ConnBtn
+            // 
+            this.ConnBtn.Location = new System.Drawing.Point(658, 44);
+            this.ConnBtn.Name = "ConnBtn";
+            this.ConnBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConnBtn.TabIndex = 0;
+            this.ConnBtn.Text = "Connect";
+            this.ConnBtn.UseVisualStyleBackColor = true;
+            this.ConnBtn.Click += new System.EventHandler(this.ConnBtn_Click);
+            // 
+            // ActivateBtn
+            // 
+            this.ActivateBtn.Location = new System.Drawing.Point(658, 195);
+            this.ActivateBtn.Name = "ActivateBtn";
+            this.ActivateBtn.Size = new System.Drawing.Size(75, 23);
+            this.ActivateBtn.TabIndex = 0;
+            this.ActivateBtn.Text = "Activate";
+            this.ActivateBtn.UseVisualStyleBackColor = true;
+            this.ActivateBtn.Click += new System.EventHandler(this.ActivateBtn_Click);
+            // 
+            // DeactivateBtn
+            // 
+            this.DeactivateBtn.Location = new System.Drawing.Point(658, 351);
+            this.DeactivateBtn.Name = "DeactivateBtn";
+            this.DeactivateBtn.Size = new System.Drawing.Size(75, 23);
+            this.DeactivateBtn.TabIndex = 0;
+            this.DeactivateBtn.Text = "Deactivate";
+            this.DeactivateBtn.UseVisualStyleBackColor = true;
+            this.DeactivateBtn.Click += new System.EventHandler(this.DeactivateBtn_Click);
+            // 
+            // EmployeeComboBox
+            // 
+            this.EmployeeComboBox.FormattingEnabled = true;
+            this.EmployeeComboBox.Location = new System.Drawing.Point(45, 44);
+            this.EmployeeComboBox.Name = "EmployeeComboBox";
+            this.EmployeeComboBox.Size = new System.Drawing.Size(121, 23);
+            this.EmployeeComboBox.TabIndex = 1;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(192, 44);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 0;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // RefreshBtn
+            // 
+            this.RefreshBtn.Location = new System.Drawing.Point(192, 142);
+            this.RefreshBtn.Name = "RefreshBtn";
+            this.RefreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.RefreshBtn.TabIndex = 0;
+            this.RefreshBtn.Text = "Refresh";
+            this.RefreshBtn.UseVisualStyleBackColor = true;
+            this.RefreshBtn.Click += new System.EventHandler(this.RefreshBtn_Click);
+            // 
+            // AssemblyLines
+            // 
+            this.AssemblyLines.FormattingEnabled = true;
+            this.AssemblyLines.ItemHeight = 15;
+            this.AssemblyLines.Location = new System.Drawing.Point(340, 195);
+            this.AssemblyLines.Name = "AssemblyLines";
+            this.AssemblyLines.Size = new System.Drawing.Size(120, 94);
+            this.AssemblyLines.TabIndex = 2;
+            // 
+            // EmployeeList
+            // 
+            this.EmployeeList.FormattingEnabled = true;
+            this.EmployeeList.ItemHeight = 15;
+            this.EmployeeList.Location = new System.Drawing.Point(45, 195);
+            this.EmployeeList.Name = "EmployeeList";
+            this.EmployeeList.Size = new System.Drawing.Size(120, 94);
+            this.EmployeeList.TabIndex = 3;
+            // 
+            // ActivatedLines
+            // 
+            this.ActivatedLines.FormattingEnabled = true;
+            this.ActivatedLines.ItemHeight = 15;
+            this.ActivatedLines.Location = new System.Drawing.Point(340, 351);
+            this.ActivatedLines.Name = "ActivatedLines";
+            this.ActivatedLines.Size = new System.Drawing.Size(120, 94);
+            this.ActivatedLines.TabIndex = 4;
+            // 
+            // Simulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ActivatedLines);
+            this.Controls.Add(this.EmployeeList);
+            this.Controls.Add(this.AssemblyLines);
+            this.Controls.Add(this.RefreshBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.EmployeeComboBox);
+            this.Controls.Add(this.DeactivateBtn);
+            this.Controls.Add(this.ActivateBtn);
+            this.Controls.Add(this.ConnBtn);
+            this.Name = "Simulation";
             this.Text = "Workstaion Simulation";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConnBtn;
+        private System.Windows.Forms.Button ActivateBtn;
+        private System.Windows.Forms.Button DeactivateBtn;
+        private System.Windows.Forms.ComboBox EmployeeComboBox;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button RefreshBtn;
+        private System.Windows.Forms.ListBox AssemblyLines;
+        private System.Windows.Forms.ListBox EmployeeList;
+        private System.Windows.Forms.ListBox ActivatedLines;
+        private System.Windows.Forms.Timer WorkSpeed;
     }
 }
 
--- Workstation Simulation/Simulation.cs
+++ Workstation Simulation/Simulation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,199 @@ namespace Workstation_Simulation
 {
     public partial class Simulation : Form
     {
+        DAL dataAccess;
+        public string Employee { get; set; }
+        public string EmployeeID { get; set; }
+        public string StationID { get; set; }
         public Simulation()
         {
             InitializeComponent();
+
+            AddBtn.Enabled = false;
+            RefreshBtn.Enabled = false;
+            ActivateBtn.Enabled = false;
+            DeactivateBtn.Enabled = false;
+        }
+        private void ConnBtn_Click(object sender, EventArgs e)
+        {
+            dataAccess = new DAL();
+            DataTable dt = new DataTable();
+            //clear the employee list
+            EmployeeComboBox.Items.Clear();
+            //clear the station list
+            AssemblyLines.Items.Clear();
+            try
+            {
+                //get employee type
+                dt = dataAccess.GetEmplyeeType();
+                foreach (DataRow row in dt.Rows)
+                {
+                    EmployeeComboBox.Items.Add(row[0].ToString());
+                }
+                dt.Clear();
+
+                //get line id and fill the combobox
+                dt = dataAccess.GetStation();
+                foreach (DataRow row in dt.Rows)
+                {
+                    AssemblyLines.Items.Add(row[0].ToString());
+                }
+                dt.Clear();
+
+
+                MessageBox.Show("Connection success");
+            }
+            catch (DALException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Refresh();
+
+            ConnBtn.Enabled = false;
+            AddBtn.Enabled = true;
+            RefreshBtn.Enabled = true;
+
+            ActivateBtn.Enabled = true;
+            DeactivateBtn.Enabled = true;
         }
 
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            EmployeeList.Items.Clear();
+            try
+            {
+                dataAccess.Hire(EmployeeComboBox.Text);
+                dt = dataAccess.GetEmlployeeList();
+                foreach (DataRow row in dt.Rows)
+                {
+                    EmployeeList.Items.Add(row[0].ToString() + ":" + row[1].ToString());
+                }
+                dt.Clear();
+            }
+
+            catch (DALException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Please check your connection string");
+            }
+        }
+
+        private void RefreshBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Refresh();
+            }
+            catch (DALException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void Refresh()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                dt = dataAccess.GetStation();
+                AssemblyLines.Items.Clear();
+                foreach (DataRow row in dt.Rows)
+                {
+                    AssemblyLines.Items.Add(row[0].ToString());
+                }
+                dt.Clear();
+
+                EmployeeList.Items.Clear();
+                dt = dataAccess.GetEmlployeeList();
+                foreach (DataRow row in dt.Rows)
+                {
+                    EmployeeList.Items.Add(row[0].ToString() + ":" + row[1].ToString());
+                }
+                dt.Clear();
+
+                ActivatedLines.Items.Clear();
+                dt = dataAccess.GetActivatedStation();
+                foreach (DataRow row in dt.Rows)
+                {
+                    ActivatedLines.Items.Add(row[0].ToString());
+                }
+                dt.Clear();
+
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Please connect first");
+            }
+
+        }
+
+        private void ActivateBtn_Click(object sender, EventArgs e)
+        {
+            string parse = EmployeeList.Text;
+            string[] employeeID = parse.Split(':');
+            EmployeeID = employeeID[0];
+            StationID = AssemblyLines.Text;
+            //check if the access and strings are emtpy
+
+            try
+            {
+                if (dataAccess != null && StationID != "" && EmployeeID != "")
+                {
+                    dataAccess.Activate(StationID, EmployeeID);
+                }
+
+                WorkSpeed.Start();
+                Refresh();
+                MessageBox.Show(StationID + " is activated");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Please connect first");
+            }
+        }
+
+        private void DeactivateBtn_Click(object sender, EventArgs e)
+        {
+            string stationID = ActivatedLines.Text;
+
+            try
+            {
+                dataAccess.Deactivate(stationID);
+                WorkSpeed.Stop();
+                Refresh();
+                MessageBox.Show(StationID + " is deactivated");
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Please connect first");
+            }
+        }
+
+        private void EmployeeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Employee = EmployeeComboBox.Text;
+        }
+
+        private void WorkSpeed_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                dataAccess.Work(StationID, EmployeeID);
+                WorkSpeed.Interval = dataAccess.GetInterval(Convert.ToInt32(StationID));
+            }
+            catch (DALException ex)
+            {
+                WorkSpeed.Stop();
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
